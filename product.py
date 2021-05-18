064b7c4154078aca58dead0d2039147484ab4a4b
Sandra <kshysei@gmail.com>
2021-05-18 18:19:40 +0900

    add find file

--- product.py
+++ product.py
@@ -1,12 +1,22 @@
+ #檢查檔案在不在
+import os
 products = []
+if os.path.isfile("product.csv"): 
+	print("耶找到檔案了")
+	with open("product.csv", "r", encoding= "utf-8") as f:
+		for line in f:
+			if "商品,價格" in line:
+				continue
+			name,price = line.strip().split(",")
+			products.append([name,price])
+	print(products)
 
-with open("products.csv", "r") as f:
-	for line in f:
-		if "商品,價格" in line:
-			continue
-		name,price = line.strip().split(",")
-		products.append([name,price])
-print(products)
+else:
+	print("沒有相關檔案")
+ 
+
+
+
 
 while True:
 	name = input("請輸入商品名稱:")
@@ -27,7 +37,7 @@ for p in products:
 	print(p[0],"的價格是", p[1])
 
 #寫入檔案
-with open("products.csv", "w",encoding="UTF-8") as f:
+with open("product.csv", "w",encoding="UTF-8") as f:
 	f.write("商品,價格\n")
 	for p in products:
 		f.write(str(p[0]) + "," + str(p[1]) + "\n")
